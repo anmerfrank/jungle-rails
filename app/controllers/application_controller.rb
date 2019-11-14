d4d6ee60d90e7cc60431cd152fd300fc6a6b5e4a
Anne Franklin <anmerfrank@gmail.com>
2019-11-14 20:24:29 +0000

    Fixed bug in login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # app/controllers/application_controller.rb
 
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
